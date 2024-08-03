5561f1775935534cac7d593f576f5d5e8ea694b8
Rakesh <rs7937753@gmail.com>
2024-08-03 19:18:19 +0530

    image indicator added

--- DemoFood/CustomView/CustomImageView.swift
+++ DemoFood/CustomView/CustomImageView.swift
@@ -8,15 +8,49 @@
 import UIKit
 
 class CustomImageView: UIImageView {
+    private var activityIndicator: UIActivityIndicatorView?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupActivityIndicator()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupActivityIndicator()
+    }
+    
+    private func setupActivityIndicator() {
+        activityIndicator = UIActivityIndicatorView(style: .medium)
+        activityIndicator?.hidesWhenStopped = true
+        if let activityIndicator = activityIndicator {
+            addSubview(activityIndicator)
+            activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                activityIndicator.centerXAnchor.constraint(equalTo: centerXAnchor),
+                activityIndicator.centerYAnchor.constraint(equalTo: centerYAnchor)
+            ])
+        }
+    }
+    
+    private func showActivityIndicator() {
+        activityIndicator?.startAnimating()
+    }
+    
+    private func hideActivityIndicator() {
+        activityIndicator?.stopAnimating()
+    }
     
     func loadImage(urlStr: String) {
-        var task: URLSessionTask!
+        var task: URLSessionTask?
         guard let url = URL(string: urlStr) else { return }
         image = nil
-        if let task = task {
-            task.cancel()
-        }
-        task = URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
+        showActivityIndicator()
+        task?.cancel()
+        task = URLSession.shared.dataTask(with: url) { data, response, error in
+            DispatchQueue.main.async {
+                self.hideActivityIndicator()
+            }
             guard let data = data, let newImage = UIImage(data: data) else {
                 print("couldn't load image from url: \(url)")
                 return
@@ -24,19 +58,21 @@ class CustomImageView: UIImageView {
             DispatchQueue.main.async {
                 self.image = newImage
             }
-        })
-        task.resume()
+        }
+        task?.resume()
     }
     
     func loadImageForIngredients(ingredient: String) {
-        var task: URLSessionTask!
+        var task: URLSessionTask?
         guard let url = URL(string: "\(ingredient).png", relativeTo: URL(string: baseURL))  else { return }
-        print("IngedientURL: ", url)
+        print("IngredientURL: ", url)
         image = nil
-        if let task = task {
-            task.cancel()
-        }
-        task = URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
+        showActivityIndicator()
+        task?.cancel()
+        task = URLSession.shared.dataTask(with: url) { data, response, error in
+            DispatchQueue.main.async {
+                self.hideActivityIndicator()
+            }
             guard let data = data, let newImage = UIImage(data: data) else {
                 print("couldn't load image from url: \(url)")
                 return
@@ -44,7 +80,7 @@ class CustomImageView: UIImageView {
             DispatchQueue.main.async {
                 self.image = newImage
             }
-        })
-        task.resume()
+        }
+        task?.resume()
     }
 }
